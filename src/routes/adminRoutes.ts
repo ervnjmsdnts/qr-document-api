e136f3e17f96ba6bd0d5d7e37c3910e83c51237f
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-11-16 00:29:48 +0800

    update

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -244,4 +244,24 @@ adminRouter.post(
   },
 );
 
+adminRouter.get(
+  'get-user',
+  async (req: TypedRequestBody<{ userId: string }>, res) => {
+    try {
+      const user = await prisma.user.findFirst({
+        where: { id: req.body.userId },
+      });
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      return res.status(200).json(user);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Failed to get user' });
+    }
+  },
+);
+
 export default adminRouter;
